154b0cd15b327ac9346592614833627f102ffe27
aliakbarzohour <aliakbar.zohour@gmail.com>
2022-10-23 01:08:55 +0330

    ...

--- .commands.sh
+++ .commands.sh
@@ -2,12 +2,13 @@
 
 function create() {
     git init
-    echo "$(tput setaf 2)" " [*] Now You Have Git ..." "$(tput sgr0)"
+    echo "$(tput setaf 2)" "[*] Now You Have Git ..." "$(tput sgr0)"
     git add -A
 
     echo " [?] What Commit ? : "
     read -n commit
     git commit -m $commit
 
+    # Clean Terminal
     clear
 }
