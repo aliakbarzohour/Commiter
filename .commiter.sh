2501cdc703fdb48bab11308c46e76423d7c21526
AliAkbar Zohour <71524940+aliakbarzohour@users.noreply.github.com>
2022-10-23 12:22:34 +0330

    Update .commiter.sh

--- .commiter.sh
+++ .commiter.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
+
 function Checkdir() {
-    # finddir=$(find . -iname ".git")
+    # check directory
     if [ -d "./.git" ]; then
         echo "$(tput setaf 2)" "[$] You Have a .git Directory " "$(tput sgr0)"
-
         # echo "directory \".git\" exists"
         git add -A
         echo " [?] What Commit ? : "
         read -n commit
         git commit -m $commit
-
     else
         git init
         git add -A
